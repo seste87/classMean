d0d9c629eddd2fca14b13c184a8ac553c7ccc836
Nicolas Hampton <nicolasjhampton@Nicolass-MacBook-Pro.local>
2016-03-27 17:22:06 -0700

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+angular.module("todoListApp", []);
+
+
+
+/*
+Note: I don't think this answer was covered in the class
+Review: get, delete, and save data with services
+In JavaScript promises are a way to manage data requested asynchronously.
+true or false question has an extra "manage data" answer option
+*/
+
+/*
+Using Filters to order ng-repeat items:
+You forgot to include the svg files for the checkboxes
+in the workspaces folders. I would add links in the teachers
+notes, or add them to the project in workspaces
+*/
+
+/*
+At the end of each section in the course, the badge modal window
+is missing the continue link, so you have to press the back
+button to get to the next section of the course.
+*/
